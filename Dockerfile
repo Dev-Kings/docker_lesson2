5a3a5f3e9bd879720d0a37bb8552e9117639da78
Dev-Kings <kingasiadavid41@gmail.com>
2023-01-16 09:15:49 +0300

    lesson 2 init

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM php:7
+ENV PORT=8000
+RUN apt-get update; apt-get install -y wget libzip-dev
+RUN docker-php-ext-install zip pdo_mysql
+RUN wget https://raw.githubusercontent.com/composer/getcomposer.org/master/web/installer -O - -q | php -- --install-dir=/usr/local/bin --filename=composer
+WORKDIR /app
+COPY . /app
+RUN composer install
+RUN touch /app/database/database.sqlite
+RUN DB_CONNECTION=sqlite php artisan migrate
+RUN DB_CONNECTION=sqlite vendor/bin/phpunit
+RUN echo "#!/bin/sh\n" \
+    "php artisan migrate\n" \
+    "php artisan serve --host 0.0.0.0 --port \$PORT" > /app/start.sh
+RUN chmod +x /app/start.sh
+CMD ["/app/start.sh"]
---
+++ app/Http/routes.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+if(version_compare(PHP_VERSION, '7.2.0', '>=')) {
+    error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+}
+
+Route::group(['middleware' => ['web']], function () {
+    /**
+     * Show Task Dashboard
+     */
+    Route::get('/', function () {
+        return view('tasks', [
+            'tasks' => Task::orderBy('created_at', 'asc')->get()
+        ]);
+    });
+
+    /**
+     * Add New Task
+     */
+    Route::post('/task', function (Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+
+        return redirect('/');
+    });
+
+    /**
+     * Delete Task
+     */
+    Route::delete('/task/{id}', function ($id) {
+        Task::findOrFail($id)->delete();
+
+        return redirect('/');
+    });
+});
